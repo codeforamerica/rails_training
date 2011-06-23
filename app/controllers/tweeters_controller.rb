c33dac92e139768607c5d70c2d4f12bd5f178223
Dan Melton <melton.dan@gmail.com>
2011-06-23 10:50:23 -0700

    Added create method

--- app/controllers/tweeters_controller.rb
+++ app/controllers/tweeters_controller.rb
@@ -6,5 +6,11 @@ class TweetersController < ApplicationController
   def new 
     @tweeter = Tweeter.new
   end
+  
+  def create
+    @tweeter = Tweeter.create(:screen_name => 
+    params[:tweeter][:screen_name])
+    redirect_to tweeters_path
+  end
 
 end
