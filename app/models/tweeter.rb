728fd484f20f8cd1cc294ecec7eeeeb5540591a9
Dan Melton <melton.dan@gmail.com>
2011-06-23 13:09:35 -0700

    Added image to index.html.erb for tweeter

--- app/models/tweeter.rb
+++ app/models/tweeter.rb
@@ -2,5 +2,6 @@ class Tweeter < ActiveRecord::Base
   has_many :tweets
   
   validates_presence_of :screen_name, :on => :create, :message => "can't be blank"
+  validates_presence_of :profile_image_url, :on => :create, :message => "can't be blank"
   validates_uniqueness_of :screen_name, :on => :create, :message => "must be unique"
 end
