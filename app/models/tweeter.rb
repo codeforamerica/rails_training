b59d4107e524042e38b3c5e987b367f53adb2ae9
Dan Melton <melton.dan@gmail.com>
2011-06-23 13:35:47 -0700

    Added get_tweets

--- app/models/tweeter.rb
+++ app/models/tweeter.rb
@@ -5,6 +5,13 @@ class Tweeter < ActiveRecord::Base
   validates_presence_of :screen_name, :on => :create, :message => "can't be blank"
   validates_uniqueness_of :screen_name, :on => :create, :message => "must be unique"
   
+  def get_tweets
+    tweets_array = Twitter.user_timeline(self.screen_name, :trim_user => true)
+    tweets_array.each do |tweet|
+      self.tweets.create(:message => tweet.text)
+    end
+  end
+
   def add_profile_image
     self.profile_image_url = Twitter.user(self.screen_name).profile_image_url
   end
