4910dad6612e46abefc2e147d6afb8c23b53e7c4
Dan Melton <melton.dan@gmail.com>
2011-06-23 13:55:08 -0700

    Added profile image and links to tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ActiveRecord::Base
   belongs_to :tweeter
-
+  default_scope :order => 'created_at DESC'
   
   validates_presence_of :tweeter_id, :on => :create, :message => "can't be blank"
   validates_presence_of :message, :on => :create, :message => "can't be blank"
