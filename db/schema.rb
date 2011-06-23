92dbec1612e0484128337a75117f7df6c7e07327
Dan Melton <melton.dan@gmail.com>
2011-06-23 10:53:51 -0700

    Removed everything so we can scaffold and make our lives easier

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110623164020) do
-
-  create_table "tweeters", :force => true do |t|
-    t.string   "screen_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
--- test/unit/helpers/tweeters_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TweetersHelperTest < ActionView::TestCase
-end
