572681e8a4bb1c5ddafe9fce8f2e9e68e1cffc28
Dan Melton <melton.dan@gmail.com>
2011-06-23 10:00:25 -0700

    Added validates uniqueness of

--- app/models/tweeter.rb
+++ app/models/tweeter.rb
@@ -1,3 +1,4 @@
 class Tweeter < ActiveRecord::Base
   validates_presence_of :screen_name, :on => :create, :message => "can't be blank"
+  validates_uniqueness_of :screen_name, :on => :create, :message => "must be unique"
 end
