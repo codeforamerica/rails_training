b5e02b8c2f15f378affbf3b110fa3c620e5480fa
Dan Melton <melton.dan@gmail.com>
2011-06-24 09:13:15 -0700

    Added migration and validation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,7 @@ class Tweet < ActiveRecord::Base
   belongs_to :tweeter
   default_scope :order => 'created_at DESC'
   
+  validates_presence_of :tweet_time, :on => :create, :message => "can't be blank"
   validates_presence_of :tweeter_id, :on => :create, :message => "can't be blank"
   validates_presence_of :message, :on => :create, :message => "can't be blank"
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110623221623) do
+ActiveRecord::Schema.define(:version => 20110624160940) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20110623221623) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "tweet_id"
+    t.datetime "tweet_time"
   end
 
 end
